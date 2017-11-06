0a35831fd740f37f6ed7319bd42c087af78cb12a
Tzu-Yin Hong <lili668668@gmail.com>
2017-11-06 23:40:22 +0800

    tmp

--- deal_with.py
+++ deal_with.py
@@ -1,5 +1,7 @@
 import const
 import sqlite3
+import time
+import datetime
 import rx
 from rx import Observable, Observer
 
@@ -28,6 +30,38 @@ Observable.from_(info) \
 # 資料轉換
 # 時間轉換成離散資料，以30分鐘為單位，並計算計算車輛的使用率：空車數量 / 總車位數
 # 將不同佔點與不同時間，車輛使用率的資料，轉換成離散資料
+
+def GetInfo(pre_time):
+    end_time = pre_time + 1800
+    select_cleaned_data_sql = 'select * from cleaned_data where time >= ? and time < ?;'
+    info = c.execute(select_cleaned_data_sql, pre_time, end_time).fetchall()
+    Observable.from_(info) \
+            .group_by(lambda item: item[1]) \
+            .
+
+
+drop_transformed_data_sql = 'drop table if exists transformed_data;'
+init_transformed_data_sql = 'create table transformed_data values(time, sno, usage real)'
+
+c.execute(drop_transformed_data_sql)
+c.execute(init_transformed_data_sql)
+
+first_time = c.execute('select time from cleaned_data order by time limit 1;').fetchall()[0]
+
+pattern = '%Y-%m-%d %H:%M:%S'
+half_hour = 1800
+tmp_time = time.strptime(first_time, pattern)
+format_str = "%Y-%m-%d %H:00:00" if tmp_time.tm_min < 30 else "%Y-%m-%d %H:30:00"
+first_time = time.strftime(format_str, tmp_time)
+first_timestamp = time.mktime(time.strptime(first_time, pattern))
+
+last_time = c.execute('select time from cleaned_data order by time desc limit 1;').fetchall()[0]
+last_timestamp = time.mktime(time.strptime(last_time, pattern)) + 1
+
+insert_transformed_data_sql = 'insert into transformed_data values(?, ?, ?);'
+info = c.execute(select_cleaned_data_sql).fetchall()
+Observable.from_(range(first_timestamp, last_timestamp, half_hour)) \
+        .subscribe(lambda pre_time: GetInfo(pre_time))
 
 # 維度轉換
 # 站點-30分鐘、一小時
