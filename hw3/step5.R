e3c4c3c987ea5a5989339259be8e11043a3384f1
Tzu-Yin Hong <lili668668@gmail.com>
2017-11-12 12:19:29 +0800

    fix old problem

---
+++ hw3/step5.R
@@ -0,0 +1,13 @@
+library(arules)
+library(Matrix)
+
+options(width=200)
+
+data <- read.table('data.csv', header = T, sep = ",")
+data <- as.matrix(data)
+
+rules <- apriori(data, parameter = list(supp = 0.2, conf = 0.6, target = "rules"))
+
+new_rules <- subset(rules, subset=lhs%in%"年齡大於25" | rhs%in%"年齡大於25")
+
+inspect(new_rules)
